bf61fe0cec881455db3d37cb76e3b09ed65cc495
leehongdae <hqtransibit@naver.com>
2023-07-20 02:35:19 +0900

    test02

--- test_react/src/App.js
+++ test_react/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({value}) {
+  return <h1>I like {value}</h1>;
+}
+
+const foodLike = [{name:"1", value:"2"},{name:"4", value:"3"}];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodLike.map(test => (<Food value={test.value}/>))}
     </div>
   );
 }
